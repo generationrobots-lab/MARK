1a6c354106c7027e41bc9b95ae7825f14b9c0fab
PF-GrLab <39522828+polofh@users.noreply.github.com>
2018-08-09 17:27:19 +0200

    add aceelero

--- Software/libraries/MARK/MARK.cpp
+++ Software/libraries/MARK/MARK.cpp
@@ -1,12 +1,13 @@
 #include "MARK.h" //include the declaration for this class
 
 Grove_LED_Bar bar(5, 4, 0);
-
+LSM6DS3 myIMU( I2C_MODE, 0x6A );  //I2C device address 0x6A
 //<<constructor>> 
 MARK::MARK(void){
 	
 	bar.begin();
 	bar.setBits(0x3ff);	
+	 myIMU.begin();
 }
  
 //<<destructor>>
@@ -17,3 +18,6 @@ void MARK::setLedBar(int level){
          bar.setLevel(level);
 }
  
+float MARK::getAccelX(){
+        return(myIMU.readFloatAccelX());
+}
--- Software/libraries/MARK/MARK.h
+++ Software/libraries/MARK/MARK.h
@@ -1,6 +1,7 @@
 #include "Arduino.h"
 #include "Wire.h"
 #include "Grove_LED_Bar.h"
+#include "SparkFunLSM6DS3.h"
 
 #ifndef MARK_H
 #define MARK_H
@@ -13,6 +14,7 @@ public:
 	MARK(void);
 	~MARK(void);
 	void setLedBar(int level);
+	float getAccelX();
 	
 	
 private:
